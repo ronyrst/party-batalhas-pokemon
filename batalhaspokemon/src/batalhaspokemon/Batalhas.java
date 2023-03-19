ad3393e3fa2250184d18f56261a1899f7bb6a4ab
ronyrst <rsantosr93@gmail.com>
2023-03-19 20:03:05 -0300

    Correção de bug e atualização para caso de empate

--- batalhaspokemon/src/batalhaspokemon/Batalhas.java
+++ batalhaspokemon/src/batalhaspokemon/Batalhas.java
@@ -10,40 +10,53 @@ public class Batalhas {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		
-		int numLutas, contadorVitoria = 0;
-		String treinadorA, treinadorB, pokemonA, pokemonB, tipoA, tipoB, letraA, letraB;
-		boolean continuar = true;
+		int numLutas, contadorVitoria = 0, contadorEmpate = 0;
+		String treinadorA = "aa", treinadorB = "bb", pokemonA, pokemonB, tipoA, tipoB, letraA, letraB;
+		boolean continuar = true, desempate = true;
 		
 		do {
-			
-			System.out.println("**********************************************");
-			System.out.println("*                                            *");
-			System.out.println("*             B A T A L H A S                *");
-			System.out.println("*              P O K É M O N                 *");
-			System.out.println("*           G E N E R A T I O N              *");
-			System.out.println("*                                            *");
-			System.out.println("**********************************************");
-			
-			System.out.println("Entre com o nome do primeiro treinador:");
-			leia.skip("\\R?");
-			treinadorA = leia.nextLine();
-			
-			System.out.println("Entre com o nome do segundo treinador:");
-			leia.skip("\\R?");
-			treinadorB = leia.nextLine();
-			
-			System.out.println("");
-			System.out.println("Quantas batalhas essa disputa terá?");
-			System.out.println("Opções:");
-			System.out.println("3  4  6  ou");
-			System.out.println("Digite 0 para sair.");
-			numLutas = leia.nextInt();
+			if(desempate) {
+				System.out.println("**********************************************");
+				System.out.println("*                                            *");
+				System.out.println("*             B A T A L H A S                *");
+				System.out.println("*              P O K É M O N                 *");
+				System.out.println("*           G E N E R A T I O N              *");
+				System.out.println("*                                            *");
+				System.out.println("**********************************************");
+				
+				System.out.println("Entre com o nome do primeiro treinador:");
+				leia.skip("\\R?");
+				treinadorA = leia.nextLine();
+				
+				System.out.println("Entre com o nome do segundo treinador:");
+				leia.skip("\\R?");
+				treinadorB = leia.nextLine();
+				
+				System.out.println("");
+				System.out.println("Quantas batalhas essa disputa terá?");
+				System.out.println("Opções:");
+				System.out.println("3  4  6  ou");
+				System.out.println("Digite 0 para sair.");
+				numLutas = leia.nextInt();
+			} else {
+				System.out.println(treinadorA + " e " + treinadorB + " empataram!");
+				
+				if(contadorEmpate > 0) {
+					System.out.println("Embates extras realizados: " + contadorEmpate);
+				} else {
+					System.out.println("A vitória agora define o vencedor:");
+				}
+				
+				numLutas = 1;
+				contadorEmpate += 1;
+			}
 			
 			if(numLutas == 0) {
 				continuar = false;
 				break;
 			}
 			
+			System.out.println("\n\n\n\n\n\n\n\n\n\n");
 			for(int i = 1; i <= numLutas; i++) {
 				
 				System.out.println("");
@@ -82,29 +95,43 @@ public class Batalhas {
 				
 				if(tipoA == tipoB) {
 					System.out.println("*          E  *  E");
+					System.out.println("*******************************");
+					System.out.println("          Empate!");
 				} 
 				else if(tipoA == "planta" && tipoB == "agua") {
 					System.out.println("*          V  *  D");
+					System.out.println("*******************************");
+					System.out.println("       " + pokemonA + " venceu!");
 					contadorVitoria += 1;
 				}
 				else if(tipoA == "planta" && tipoB == "fogo") {
 					System.out.println("*          D  *  V");
+					System.out.println("*******************************");
+					System.out.println("       " + pokemonB + " venceu!");
 					contadorVitoria += -1;
 				}
 				else if(tipoA == "agua" && tipoB == "fogo") {
 					System.out.println("*          V  *  D");
+					System.out.println("*******************************");
+					System.out.println("       " + pokemonA + " venceu!");
 					contadorVitoria += 1;
 				}
 				else if(tipoA == "agua" && tipoB == "planta") {
 					System.out.println("*          D  *  V");
+					System.out.println("*******************************");
+					System.out.println("       " + pokemonB + " venceu!");
 					contadorVitoria += -1;
 				}
 				else if(tipoA == "fogo" && tipoB == "planta") {
 					System.out.println("*          V  *  D");
+					System.out.println("*******************************");
+					System.out.println("       " + pokemonA + " venceu!");
 					contadorVitoria += 1;
 				}
 				else if(tipoA == "fogo" && tipoB == "agua") {
 					System.out.println("*          D  *  V");
+					System.out.println("*******************************");
+					System.out.println("       " + pokemonB + " venceu!");
 					contadorVitoria += -1;
 				}
 				
@@ -113,10 +140,14 @@ public class Batalhas {
 			
 			
 			if(contadorVitoria == 0) {
-				System.out.println("");
-				System.out.println("*******************************");
-				System.out.println("  A batalha acabou em empate!  ");
-				System.out.println("*******************************");
+				if(desempate) {
+					System.out.println("");
+					System.out.println("*******************************");
+					System.out.println("  A batalha acabou em empate!  ");
+					System.out.println("*******************************");	
+					
+					desempate = false;
+				}
 			}
 			else {
 				System.out.println("");
@@ -129,16 +160,25 @@ public class Batalhas {
 					System.out.println("            " + treinadorB);
 				
 				System.out.println("*******************************");
+				
+				desempate = true;
+				contadorEmpate = 0;
 			}
-			keyPress();
+			
+			contadorVitoria = 0;
+			if(desempate)
+				keyPress();
+			System.out.println("\n\n\n\n\n");
 			
 		} while(continuar);
+		
+		leia.close();
 	}
 	
 	
 	public static void keyPress() { 
 		try { 
-			System.out.println("\n\nPressione Enter para Continuar...");
+			System.out.println("\n\nPressione Enter para Continuar\n\n\n\n\n");
 			System.in.read(); 
 		} catch (IOException e) {
 			System.out.println("Você pressionou uma tecla diferente de enter!");
@@ -148,12 +188,12 @@ public class Batalhas {
 	
 	public static String defineTipo(String letra) {
 		List<String> tipos = Arrays.asList(
-				"fogo","planta","planta","agua","fogo",
-				"planta","planta","agua","agua","agua",
-				"agua","agua","planta","agua","agua",
-				"fogo","agua","planta","agua","agua",
-				"fogo","planta","planta","planta","planta",
-				"fogo"
+				"planta","agua","planta","planta","fogo",
+				"planta","agua","fogo","planta","planta",
+				"fogo","agua","planta","planta","planta",
+				"agua","fogo","fogo","planta","agua",
+				"fogo","fogo","fogo","fogo","fogo",
+				"agua"
 				);
 		String letraMinuscula;
 		
